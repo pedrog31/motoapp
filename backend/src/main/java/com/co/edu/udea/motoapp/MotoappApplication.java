ed3b2ec54c00b87619bc78fc79b4e9efbac41f93
Pedro Gallego <pedro.gallego@udea.edu.co>
2018-09-03 21:41:18 -0500

    init project

---
+++ backend/src/main/java/com/co/edu/udea/motoapp/MotoappApplication.java
@@ -0,0 +1,12 @@
+package com.co.edu.udea.motoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoappApplication.class, args);
+	}
+}
