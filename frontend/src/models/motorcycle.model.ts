b3f41527f93f8b7ec5a9706743bdc88ecfbfae36
pedro  gallego <desarrolloingenieria15@udea.edu.co>
2019-02-25 01:33:22 -0500

    heroku deploy

--- frontend/src/models/motorcycle.model.ts
+++
@@ -1,7 +0,0 @@
-export class MotorcycleModel {
-  _id: string;
-  name: string;
-  brand: string;
-  cyl: string;
-  uri: string;
-}
--- frontend/src/models/usermotorcycle.model.ts
+++
@@ -1,7 +0,0 @@
-export class UserMotorcycleModel {
-  _idMotorcycle: string;
-  soat: Date;
-  tm: Date;
-  color: string;
-  km: number;
-}
